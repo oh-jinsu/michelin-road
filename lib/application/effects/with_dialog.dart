2d40c9275546007b4e221c1b59fc116a57b9b2f8
오진수 <ohjinsu@codersproduct.com>
2022-06-03 22:37:20 +0900

    Add the cuperitno alert dialog

--- lib/application/effects/with_dialog.dart
+++ lib/application/effects/with_dialog.dart
@@ -1,27 +1,50 @@
+import 'dart:io';
+
 import 'package:codux/codux.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 mixin DialogEffectMixin on Effect {
   void showAlertDialog({
     required String content,
   }) {
-    showDialog(
-      context: requireContext(),
-      barrierDismissible: false,
-      builder: (context) {
-        return AlertDialog(
-          title: const Text("안내"),
-          content: Text(content),
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
-              child: const Text("확인"),
-            )
-          ],
-        );
-      },
-    );
+    if (Platform.isIOS) {
+      showCupertinoDialog(
+        context: requireContext(),
+        builder: (context) {
+          return CupertinoAlertDialog(
+            title: const Text("안내"),
+            content: Text(content),
+            actions: [
+              CupertinoButton(
+                child: const Text("확인"),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              )
+            ],
+          );
+        },
+      );
+    } else {
+      showDialog(
+        context: requireContext(),
+        barrierDismissible: false,
+        builder: (context) {
+          return AlertDialog(
+            title: const Text("안내"),
+            content: Text(content),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+                child: const Text("확인"),
+              )
+            ],
+          );
+        },
+      );
+    }
   }
 }
